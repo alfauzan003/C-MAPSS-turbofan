678f48fb2eeebb92aada568c735f807aa9c5baa7
alfauzan003 <habibalfauzan1@gmail.com>
2026-04-25 10:54:47 +0700

    fix(features): remove try/except guard from features __init__; add compute_windows in Task 2

--- pdm/features/__init__.py
+++ pdm/features/__init__.py
@@ -6,9 +6,4 @@ to guarantee the same transforms apply on both sides.
 
 from pdm.features.rul import compute_rul
 
-try:
-    from pdm.features.windows import compute_windows
-except ImportError:
-    pass
-
-__all__ = ["compute_rul", "compute_windows"]
+__all__ = ["compute_rul"]
